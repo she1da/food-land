a140dab7cd7e0a9d658520649c526193b9c29604
Sheyda <sheydaahmadipour@gmail.com>
2025-01-04 01:47:10 +0330

    services added

---
+++ src/app/components/services.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import Image from "next/image";
+
+export default function Services(): JSX.Element {
+  return (
+    <section className=" flex flex-wrap w-full items-center justify-between mx-auto p-4  pt-10">
+      <div className="w-full  items-center justify-center">
+        <span>Product</span>
+        <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Most Popular Items
+        </h2>
+      </div>
+
+      <div>
+        <div className="grid  grid-cols-3  justify-items-center">
+          <div className="p-6 m-6 justify-self-auto">
+            <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <div className="flex justify-end px-4 pt-4"></div>
+              <div className="flex flex-col items-center pb-10">
+                <Image
+                  className=" w-24 h-24 mb-3 rounded-full shadow-lg"
+                  src="/Ellipse 10.png"
+                  alt="Next.js logo"
+                  width={98}
+                  height={98}
+                  priority
+                />
+                <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                  Bonnie Green
+                </h5>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Visual Designer
+                </span>
+                <div className="flex mt-4 md:mt-6"></div>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 m-6 justify-self-auto">
+            <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <div className="flex justify-end px-4 pt-4"></div>
+              <div className="flex flex-col items-center pb-10">
+                <Image
+                  className=" w-24 h-24 mb-3 rounded-full shadow-lg"
+                  src="/Ellipse 10.png"
+                  alt="Next.js logo"
+                  width={98}
+                  height={98}
+                  priority
+                />
+                <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                  Bonnie Green
+                </h5>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Visual Designer
+                </span>
+                <div className="flex mt-4 md:mt-6"></div>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 m-6 justify-self-auto">
+            <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <div className="flex justify-end px-4 pt-4"></div>
+              <div className="flex flex-col items-center pb-10">
+                <Image
+                  className=" w-24 h-24 mb-3 rounded-full shadow-lg"
+                  src="/Ellipse 10.png"
+                  alt="Next.js logo"
+                  width={98}
+                  height={98}
+                  priority
+                />
+                <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                  Bonnie Green
+                </h5>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Visual Designer
+                </span>
+                <div className="flex mt-4 md:mt-6"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,19 @@ import Image from "next/image";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Card from "./components/card";
+import Info from "./components/info";
+import Services from "./components/services";
 
 export default function Home() {
   return (
-    <div >
-         <Header /> 
- <main className="flex ">
-        <Hero/>
-   
-      
+    <div>
+      <main>
+        <Hero />
+        <Info />
+        <Card />
+        <Services />
       </main>
-      <Footer />  
-
     </div>
-
   );
 }
