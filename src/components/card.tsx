6473ec6745420ae1182286ebaa4ab0677cad1e8f
Sheyda <sheydaahmadipour@gmail.com>
2025-01-11 23:36:16 +0330

    counter added to menu

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
+import { useContext } from "react";
+import { CounterContext } from "@/components/shoppingCart";
 
 export default function Card(): JSX.Element {
+  const [counts, setCounts] = useState({
+    gyro: 0,
+    enchilada: 0,
+    greenBeans: 0,
+    pizza: 0,
+    chickenPotPie: 0,
+    greenSalad: 0,
+  });
+
+  const addToCart = (item: string) => {
+    setCounts((prevCounts) => ({
+      ...prevCounts,
+      [item]: prevCounts[item] + 1,
+    }));
+  };
+
+  const removeFromCart = (item: string) => {
+    setCounts((prevCounts) => ({
+      ...prevCounts,
+      [item]: Math.max(prevCounts[item] - 1, 0),
+    }));
+  };
+
   return (
     <section className=" flex flex-wrap w-full items-center justify-between mx-auto p-4  pt-10">
       <div className="w-full md:w-12/12 items-center justify-center">
@@ -14,7 +40,10 @@ export default function Card(): JSX.Element {
       <div className="w-full md:w-12/12">
         <div className="grid  grid-cols-3 gap-x-4 gap-y-4 ">
           <div className="justify-self-auto">
-            <div className="max-w-md bg-white  shadow dark:bg-gray-800 dark:border-gray-700 rounded-3xl">
+            <div
+              className="max-w-md bg-white  shadow dark:bg-gray-800 dark:border-gray-700 rounded-3xl"
+              id="gyro-sandwich"
+            >
               <a href="#">
                 <Image
                   className=" rounded-t-3xl "
@@ -33,11 +62,27 @@ export default function Card(): JSX.Element {
                     Gyro Sandwhich
                   </h5>
                 </a>
+                <button
+                  onClick={() => removeFromCart("gyro")}
+                  className="mt-2 bg-red-500 text-white py-2 px-4 rounded"
+                >
+                  -
+                </button>
+                <span className="mx-2">{counts.gyro}</span>
+                <button
+                  onClick={() => addToCart("gyro")}
+                  className="mt-2 bg-blue-500 text-white py-2 px-4 rounded"
+                >
+                  +
+                </button>
               </div>
             </div>
           </div>
           <div className="">
-            <div className="max-w-md bg-white  shadow dark:bg-gray-800 dark:border-gray-700 rounded-3xl">
+            <div
+              className="max-w-md bg-white  shadow dark:bg-gray-800 dark:border-gray-700 rounded-3xl"
+              id="enchilada"
+            >
               <a href="#">
                 <Image
                   className=" rounded-t-3xl "
@@ -56,11 +101,27 @@ export default function Card(): JSX.Element {
                     Enchilade
                   </h5>
                 </a>
+                <button
+                  onClick={() => removeFromCart("enchilada")}
+                  className="mt-2 bg-red-500 text-white py-2 px-4 rounded"
+                >
+                  -
+                </button>
+                <span className="mx-2">{counts.enchilada}</span>
+                <button
+                  onClick={() => addToCart("enchilada")}
+                  className="mt-2 bg-blue-500 text-white py-2 px-4 rounded"
+                >
+                  +
+                </button>
               </div>
             </div>
           </div>
           <div className="">
-            <div className="max-w-md bg-white  shadow dark:bg-gray-800 dark:border-gray-700 rounded-3xl">
+            <div
+              className="max-w-md bg-white  shadow dark:bg-gray-800 dark:border-gray-700 rounded-3xl"
+              id="green-beans"
+            >
               <a href="#">
                 <Image
                   className=" rounded-t-3xl "
@@ -79,11 +140,27 @@ export default function Card(): JSX.Element {
                     Green Beans
                   </h5>
                 </a>
+                <button
+                  onClick={() => removeFromCart("greenBeans")}
+                  className="mt-2 bg-red-500 text-white py-2 px-4 rounded"
+                >
+                  -
+                </button>
+                <span className="mx-2">{counts.greenBeans}</span>
+                <button
+                  onClick={() => addToCart("greenBeans")}
+                  className="mt-2 bg-blue-500 text-white py-2 px-4 rounded"
+                >
+                  +
+                </button>
               </div>
             </div>
           </div>
           <div className="">
-            <div className="max-w-md bg-white  shadow dark:bg-gray-800 dark:border-gray-700 rounded-3xl">
+            <div
+              className="max-w-md bg-white  shadow dark:bg-gray-800 dark:border-gray-700 rounded-3xl"
+              id="pizza"
+            >
               <a href="#">
                 <Image
                   className=" rounded-t-3xl "
@@ -102,11 +179,27 @@ export default function Card(): JSX.Element {
                     Pizza
                   </h5>
                 </a>
+                <button
+                  onClick={() => removeFromCart("pizza")}
+                  className="mt-2 bg-red-500 text-white py-2 px-4 rounded"
+                >
+                  -
+                </button>
+                <span className="mx-2">{counts.pizza}</span>
+                <button
+                  onClick={() => addToCart("pizza")}
+                  className="mt-2 bg-blue-500 text-white py-2 px-4 rounded"
+                >
+                  +
+                </button>
               </div>
             </div>
           </div>
           <div className="">
-            <div className="max-w-md bg-white  shadow dark:bg-gray-800 dark:border-gray-700 rounded-3xl">
+            <div
+              className="max-w-md bg-white  shadow dark:bg-gray-800 dark:border-gray-700 rounded-3xl"
+              id="chicken-pot-pie"
+            >
               <a href="#">
                 <Image
                   className=" rounded-t-3xl "
@@ -125,11 +218,27 @@ export default function Card(): JSX.Element {
                     Chicken Pot Pie
                   </h5>
                 </a>
+                <button
+                  onClick={() => removeFromCart("chickenPotPie")}
+                  className="mt-2 bg-red-500 text-white py-2 px-4 rounded"
+                >
+                  -
+                </button>
+                <span className="mx-2">{counts.chickenPotPie}</span>
+                <button
+                  onClick={() => addToCart("chickenPotPie")}
+                  className="mt-2 bg-blue-500 text-white py-2 px-4 rounded"
+                >
+                  +
+                </button>
               </div>
             </div>
           </div>
           <div className="">
-            <div className="max-w-md bg-white  shadow dark:bg-gray-800 dark:border-gray-700 rounded-3xl">
+            <div
+              className="max-w-md bg-white  shadow dark:bg-gray-800 dark:border-gray-700 rounded-3xl"
+              id="green-salad"
+            >
               <a href="#">
                 <Image
                   className=" rounded-t-3xl "
@@ -148,6 +257,19 @@ export default function Card(): JSX.Element {
                     Green Salad
                   </h5>
                 </a>
+                <button
+                  onClick={() => removeFromCart("greenSalad")}
+                  className="mt-2 bg-red-500 text-white py-2 px-4 rounded"
+                >
+                  -
+                </button>
+                <span className="mx-2">{counts.greenSalad}</span>
+                <button
+                  onClick={() => addToCart("greenSalad")}
+                  className="mt-2 bg-blue-500 text-white py-2 px-4 rounded"
+                >
+                  +
+                </button>
               </div>
             </div>
           </div>
